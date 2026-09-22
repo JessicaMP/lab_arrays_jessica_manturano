0f5b4d36245d837c1b5b380c5b905581e8fe8f95
JessicaMP <jessica95.mp@gmail.com>
2026-09-22 11:29:07 -0500

    complete task 6, pass all test

--- task6.js
+++ task6.js
@@ -2,7 +2,8 @@ const arr1 = ["hello"]
 const arr2 = ["world"]
 
 // ¡Escribe tu código debajo de esta línea!
-
+const newArr = arr1.concat(arr2)
 
 
 // ¡Escribe tu código encima de esta línea!
+console.log(newArr);
